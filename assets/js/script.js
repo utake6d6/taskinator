94eb3725ea47c7499a6b33b58883a49e4e1a9711
Bert <robertgill82@gmail.com>
2020-10-13 20:28:07 -0700

    form submission working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-}
+var createTaskHandler = function(event) {
 
-buttonEl.addEventListener("click", createTaskHandler() {
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-});
+};
+
+formEl.addEventListener("submit", createTaskHandler); {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
 
